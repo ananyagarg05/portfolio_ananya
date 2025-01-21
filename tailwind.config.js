23c6a9c365584192c35eea70bad370436e733ae3
ananyagarg05 <itsananyagarg05@gmail.com>
2025-01-21 21:10:53 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,33 +7,69 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ], // Paths to scan for Tailwind CSS classes
   theme: {
-    container: {
-      center: true,
-      padding: "15px", // Adds padding to the container
-    },
-    screens: {
-      sm: "640px", // Small screen breakpoint
-      md: "768px", // Medium screen breakpoint
-      lg: "960px", // Large screen breakpoint
-      xl: "1200px", // Extra large screen breakpoint
-    },
-    fontFamily: {
-      primary: "var(--font-jetbrainsMono)", // Custom font using a CSS variable
-    },
-    extend: {
-      colors: {
-        primary: "#1c1c22", // Custom primary color
-        accent: {
-          DEFAULT: "#00ff99", // Default accent color
-          hover: "#00e187", // Accent color on hover
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)", // Large border radius
-        md: "calc(var(--radius) - 2px)", // Medium border radius
-        sm: "calc(var(--radius) - 4px)", // Small border radius
-      },
-    },
+  	container: {
+  		center: true,
+  		padding: '15px'
+  	},
+  	screens: {
+  		sm: '640px',
+  		md: '768px',
+  		lg: '960px',
+  		xl: '1200px'
+  	},
+  	fontFamily: {
+  		primary: 'var(--font-jetbrainsMono)'
+  	},
+  	extend: {
+  		colors: {
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				hover: '#00e187',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")], // Adds the tailwindcss-animate plugin
 };
